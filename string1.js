b83c767542a4a33fed4032acf0ec026d4dc46fe5
Sagar Haldhar <sagarhaldhar76@gmail.com>
2025-08-31 23:06:43 +0530

    introduces JS string part 1 and 2

---
+++ string1.js
@@ -0,0 +1,23 @@
+// Declare and initialization
+// String can be enclosed by '' , "" and ``
+let text1 = "Hello";
+console.log(text1); // Hello
+
+let text2 = "Dear Nikita";
+console.log(text2); // Dear Nikita
+
+let text3 = `Hello From Sagar`;
+console.log(text3); // Hello From Sagar
+
+// Length of a string
+console.log(text1.length); // 5
+
+// Concatenation of string (Strings ko Jodna)
+
+// If we want to combine the strings and assign it to a new variable
+const text4 = text1 + " " + text2 + " " + text3;
+console.log(text4); // Hello Dear Nikita Hello From Sagar
+
+// If we only needs to print the combined string we can use comma (,)
+// Comma ka ek nature hai ki wo agli value print karne se pehle ek space by default de deta hai
+console.log(text1, text2, text3); // Hello Dear Nikita Hello From Sagar
---
+++ string2.js
@@ -0,0 +1,18 @@
+// Value insertion in a string
+let x = 5;
+let y = 10;
+console.log(`The sum of ${x} and ${y} is ${x + y}`); // The sum of 5 and 10 is 15
+
+// Difference between == and ===
+// == compares only value
+// During == comparison if one of a value is string, then JS convert another value into string and then convert them
+// === compares datatype as well as values
+
+let var1 = 3; // "3"  (during comparison with string, JS internally convert it into a string)
+let var2 = "3";
+let var3 = true; // "true" (during comparison with string, JS internally convert it into a string)
+let var4 = "true";
+console.log(var1 == var2); // value is compared Ans. True
+console.log(var1 === var2); // datatype and value both are checked Ans. False
+console.log(var3 == var4); // Ans.True
+console.log(var3 === var4); // Ans. False
